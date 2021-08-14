b86a97af33c12b380880179cf4108b41a8dd9681
guoweiz <zhaoguowei.tepper@gmail.com>
2021-08-14 16:50:42 -0400

    Add comparison commands

--- myScript.sh
+++ myScript.sh
@@ -38,3 +38,13 @@ then
 else
     echo "Your name is NOT Brad or Jack"
 fi
+
+# Comparison
+######
+# val1 -eq val2 Returns true if the values are equal
+# val1 -ne val2 Returns true if the values are not equal
+# val1 -gt val2 Ruturns true if val1 is greater than val2
+# val1 -ge val2 Returns true if val2 is greater than or equal to val2
+# val1 -lt val2 Returns true if val1 is less than val2
+# val1 -le val2 Returns true if val1 is less than or equal to val2
+######
